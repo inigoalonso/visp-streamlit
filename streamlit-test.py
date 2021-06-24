249416f7698af45dfcce872af3aa5e5958c5a70e
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2021-06-24 08:09:06 +0200

    tests

--- streamlit-test.py
+++ streamlit-test.py
@@ -1,21 +1,21 @@
 import streamlit as st
-import time
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.sankey import Sankey
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
+fig = plt.figure(figsize = [10,10])
+ax = fig.add_subplot(1,1,1)
 
-for i in range(1, 201):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(int(i/2))
-    last_rows = new_rows
-    time.sleep(0.05)
+Sankey(ax=ax,  flows = [ 20400,3000,-19900,-400,-2300,-800],
+              labels = ['K',   'S', 'H',   'F', 'Sp',  'x'],
+        orientations = [ 1,    -1,   1,     0,   -1,   -1 ],
+        scale=1/25000, trunklength=1,
+        edgecolor = '#099368', facecolor = '#099368'
+      ).finish()
+plt.axis("off")
+#plt.show()
+st.plotly_chart(fig)
 
-progress_bar.empty()
 
 # Streamlit widgets automatically run the script from top to bottom. Since
 # this button is not connected to any other logic, it just causes a plain
