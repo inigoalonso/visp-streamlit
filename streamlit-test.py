bd6c197bf952bdc89f5d47caeeb20541c8e3a1d4
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2021-06-30 14:39:46 +0200

    Update streamlit-test.py

--- streamlit-test.py
+++ streamlit-test.py
@@ -2,19 +2,40 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.sankey import Sankey
+import pandas as pd
 
-fig = plt.figure(figsize = [10,10])
-ax = fig.add_subplot(1,1,1)
+st.title('Uber pickups in NYC')
 
-Sankey(ax=ax,  flows = [ 20400,3000,-19900,-400,-2300,-800],
-              labels = ['K',   'S', 'H',   'F', 'Sp',  'x'],
-        orientations = [ 1,    -1,   1,     0,   -1,   -1 ],
-        scale=1/25000, trunklength=1,
-        edgecolor = '#099368', facecolor = '#099368'
-      ).finish()
-plt.axis("off")
-#plt.show()
-st.plotly_chart(fig)
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading data...')
+data = load_data(10000)
+data_load_state.text("Done! (using st.cache)")
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+st.subheader('Number of pickups by hour')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+
+# Some number in the range 0-23
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
 
 
 # Streamlit widgets automatically run the script from top to bottom. Since
